a6f06025bfaff90337b11e40d45be7883903f3e5
strbrgr <jocsti@web.de>
2025-03-05 18:32:36 -0600

    feat: TS strict mode

--- app/rss/route.ts
+++ app/rss/route.ts
@@ -2,7 +2,7 @@ import { baseUrl } from 'app/sitemap'
 import { getBlogPosts } from 'app/blog/utils'
 
 export async function GET() {
-  let allBlogs = await getBlogPosts()
+  let allBlogs = getBlogPosts()
 
   const itemsXml = allBlogs
     .sort((a, b) => {
@@ -18,8 +18,8 @@ export async function GET() {
           <link>${baseUrl}/blog/${post.slug}</link>
           <description>${post.metadata.summary || ''}</description>
           <pubDate>${new Date(
-            post.metadata.publishedAt
-          ).toUTCString()}</pubDate>
+          post.metadata.publishedAt
+        ).toUTCString()}</pubDate>
         </item>`
     )
     .join('\n')
